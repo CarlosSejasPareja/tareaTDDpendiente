b4735b6d58ffdf82f8c8dd31319cffcf85832e9a
Carlos Sejas <carlos.sejas@ucb.edu.bo>
2023-08-27 13:16:38 -0400

    es bisiesto si es divisible entre 400

--- src/esBisiesto.js
+++ src/esBisiesto.js
@@ -1,9 +1,19 @@
-function esBisiesto(anio){
-    if (anio===4){
-        return true;
+class anio{
+    constructor(anio){
+        this.anio=anio;
     }
-    return false;
-    
- }
+    esBisiesto(){
+        if(this.anioesDivisiblePor(400)) return true;
+        if (this.anioesDivisiblePor(4) && this.anioNoEsDivisiblePor(100)) return  true ;
+        return false;
+    }
 
- export default esBisiesto;
+    anioesDivisiblePor(numero){
+        return this.anio % numero == 0;
+    }
+    
+    anioNoEsDivisiblePor(numero){
+        return this.anio % numero !=0;
+    }
+}
+export default anio
--- src/esBisiesto.spec.js
+++ src/esBisiesto.spec.js
@@ -1,14 +1,24 @@
-import esBisiesto from "./esBisiesto.js";
+import anio from "./esBisiesto.js";
 
 describe("Es Bisiesto", () => {
   it("el 4 deberia considerarse como anio bisiesto", () => {
-    expect(esBisiesto(4)).toEqual(true);
+    expect(anioBisiesto(4)).toEqual(true);
   });
 
   it("el 5 no deberia considerarse como anio bisiesto", () => {
-
-    expect(esBisiesto(5)).toEqual(false);
+    expect(anioBisiesto(5)).toEqual(false);
   });
+
+  it("es bisiesto si es divisible entre 400", () => {
+    expect(anioBisiesto(400)).toEqual(true);
+  });
+
+  
+
 });
+
+function anioBisiesto(a){
+    return new anio (a).esBisiesto();
+}
 
  
